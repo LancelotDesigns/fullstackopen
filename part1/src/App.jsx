2fb28e9b39cef8e7c6ef72675cf841b0c8ba66ad
Lance <175714111+LancelotDesigns@users.noreply.github.com>
2026-08-29 12:29:34 +0200

    1.2: extract Part component, Content renders three Parts

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,31 @@
-// header component
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   )
 }
 
-// content component
 const Content = (props) => {
   return ( 
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
   )
 }
 
-
-// Total component
 const Total = (props) => {
   return (
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+
+// 1-2 Part Component
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+
   )
 }
 
@@ -41,8 +46,7 @@ const App = () => {
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
       />
-      <Total exercises1={exercises1}  exercises2={exercises2} exercises3={exercises3}/>
-  
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
